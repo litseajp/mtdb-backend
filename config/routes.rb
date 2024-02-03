c79f3564e0b346da520267bf2adf623567826168
Litsea <litsea52@gmail.com>
2024-02-03 20:12:52 +0900

    add: scalesコントローラーにshowアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :scales, only: [:index]
+  resources :scales, only: [:index, :show], param: :scale
 end
