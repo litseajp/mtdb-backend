84a844c264fd58d29b3aad989b0b157cfe42adff
Litsea <litsea52@gmail.com>
2024-02-11 03:42:40 +0900

    add: scalesコントローラーにshowアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   resources :scales, only: [:index, :show], param: :scale
 
-  resources :chords, only: [:index]
+  resources :chords, only: [:index, :show], param: :chord
 end
